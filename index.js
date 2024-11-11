107e0c103759ecad84f36b38212fe1aac23a662f
jagatheeswaran <srijagatheeswaran@gmail.com>
2024-11-12 01:56:11 +0530

    allow all the link

--- index.js
+++ index.js
@@ -13,22 +13,15 @@ const DB = process.env.DATABASE
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
-const allowedOrigins = [
-    'http://localhost:3000', 
-    process.env.FRONTEND_URL  
-];
+// const allowedOrigins = [
+//     'http://localhost:3000', 
+//     process.env.FRONTEND_URL  
+// ];
 // app.use(cors({
 //     origin: 'http://localhost:3000'
 // }));
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    }
+    origin: '*'
 }));
 mongoose.connect(DB, {
     useNewUrlParser: true,
