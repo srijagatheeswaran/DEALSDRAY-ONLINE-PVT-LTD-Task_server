3a26d8de12ba42f67ea4db75a9efd753797d00c7
jagatheeswaran <srijagatheeswaran@gmail.com>
2024-11-12 01:01:10 +0530

    add cors

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 
-const express =require('express')
-const mongoose = require('mongoose'); 
+const express = require('express')
+const mongoose = require('mongoose');
 const cors = require('cors');
 
-const adminRoutes=require("./routes/adminRoutes")
-const employeeRoutes=require("./routes/employeeRoutes")
+const adminRoutes = require("./routes/adminRoutes")
+const employeeRoutes = require("./routes/employeeRoutes")
 
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT;
-const DB=process.env.DATABASE
+const DB = process.env.DATABASE
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
+// app.use(cors({
+//     origin: 'http://localhost:3000'
+// }));
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
 }));
-
 mongoose.connect(DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
